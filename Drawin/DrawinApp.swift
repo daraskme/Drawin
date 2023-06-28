3a98d0172b08c0f171e731a3abc76cad09651121
DARASKdot <102633287+DARASKdot@users.noreply.github.com>
2023-06-28 11:42:40 +0900

    Initial Commit

---
+++ Drawin/DrawinApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawinApp.swift
+//  Drawin
+//
+//  Created by Hiroshi Inoue on R 5/06/28.
+//
+
+import SwiftUI
+
+@main
+struct DrawinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
